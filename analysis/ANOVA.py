f3d623649535cefac3bd6da4161ff58a8d8d30b4
陈雪泽 <116641047+AXiaoming666@users.noreply.github.com>
2025-05-23 10:00:31 +0800

    commit

--- analysis/ANOVA.py
+++ analysis/ANOVA.py
@@ -7,6 +7,7 @@ matplotlib.use('Agg')
 import seaborn as sns
 from scipy.stats import pearsonr
 import os
+import re
 
 save_path = './visualization/ANOVA/'
 os.makedirs(save_path, exist_ok=True)
@@ -21,17 +22,48 @@ def explained_deviation(anova_table):
     return explained_deviations
 
 def plot_explained_deviation(partial_etas, title):
-    factors = list(partial_etas.keys())
-    eta_values = list(partial_etas.values())
+    # 过滤出 deviance 大于 0.002 的数据
+    filtered_etas = {factor: value for factor, value in partial_etas.items() if value > 0.002}
+    factors = list(filtered_etas.keys())
+    eta_values = list(filtered_etas.values())
+    
+    factors = [re.sub(r'C\((.*?)\)', r'\1', factor) for factor in factors]
+    factors = [factor.replace(':', ':\n') for factor in factors]
     
     combined = list(zip(factors, eta_values))
     combined.sort(key=lambda x: x[1], reverse=True)
     factors, eta_values = zip(*combined)
 
-    plt.figure(figsize=(10, 6))
-    plt.barh(factors, eta_values, color='blue')
-    plt.ylabel('Factors')
-    plt.xlabel('Deviance')
+    plt.figure(figsize=(16, 6))
+    
+    # 根据柱子索引归一化，生成从深到浅的颜色值
+    num_bars = len(factors)
+    normalized_indices = [(num_bars - i - 1) / (num_bars - 1) for i in range(num_bars)]
+    
+    # 生成同一色系（蓝色系）的颜色列表，从左到右颜色逐渐变浅
+    colors = [(0, 0, value) for value in normalized_indices]
+    
+    # 设置 align 为 edge，width 为负数，让柱子向左绘制，使用同一色系颜色
+    bars = plt.bar(factors, eta_values, color=colors, align='center', width=0.4)
+    plt.xlabel('Factors')
+    plt.ylabel('Deviance')
+
+    # 移除边框
+    ax = plt.gca()
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    # 添加浅色网格线
+    plt.grid(True, axis='y', color='lightgray', linestyle='--', alpha=0.7)
+
+    # 调整标签位置以适应垂直柱状图
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, height, f'{height:.3f}', 
+                 ha='center', va='bottom', color='black', fontsize=10)
+
+    # 调整 x 轴标签位置，使其尾部对准柱体右侧
+    plt.xticks(rotation=45, ha='center')
     plt.tight_layout()
     plt.savefig(f'{save_path}{title.replace(" ", "_")}.png')
     plt.close()
@@ -70,11 +102,6 @@ for dataset in datasets:
 
     # 计算相关系数
     corr_matrix, p_value_matrix = calculate_pearson_corr(result)
-    print(f"Pearson Correlation Matrix for {dataset}:")
-    print(corr_matrix)
-    print(f"P-Value Matrix for {dataset}:")
-    print(p_value_matrix)
-
     # 绘制相关系数热力图
     plot_corr_heatmap(corr_matrix, f'Pearson Correlation between Imputation and Forecast Metrics in {dataset}')
 
